9472888d0d715a6e676684a18ab0291bf236d441
SamVa <39919617+SamVaZlo@users.noreply.github.com>
2020-07-29 12:19:52 +0200

    Update Lambda_Function.py

--- Lambda_Function.py
+++ Lambda_Function.py
@@ -1,6 +1,7 @@
 import json
 
 def lambda_handler(event, context):
+    #Input
     text = event['startingText']
     replaceList = [ 'Oracle','Oracle©','Google','Google©','Microsoft','Microsoft©','Amazon','Amazon©','Deloitte','Deloitte©','test']
     
@@ -8,8 +9,11 @@ def lambda_handler(event, context):
     if len(replaceList)%2 != 0:
         replaceList.append(replaceList[-1]) 
         
+    #Replace undesired parts of the string
     for i in range(0, len(replaceList), 2):
         text = text.replace(replaceList[i],replaceList[i+1])
+        
+    #Output
     return {
         'statusCode': 200,
         'body': json.dumps(text)
