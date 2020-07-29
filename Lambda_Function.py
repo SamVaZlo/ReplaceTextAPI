9856c0718a21207ac540cf5987a53b8cf227f846
SamVa <39919617+SamVaZlo@users.noreply.github.com>
2020-07-29 11:33:51 +0200

    Update Lambda_Function.py

--- Lambda_Function.py
+++ Lambda_Function.py
@@ -2,8 +2,12 @@ import json
 
 def lambda_handler(event, context):
     text = event['startingText']
-    replaceList = ['Oracle','Oracle©','Google','Google©','Microsoft','Microsoft©','Amazon','Amazon©','Deloitte','Deloitte©']
-
+    replaceList = [ 'Oracle','Oracle©','Google','Google©','Microsoft','Microsoft©','Amazon','Amazon©','Deloitte','Deloitte©','test']
+    
+    #prevent error if amount of items i uneven
+    if len(replaceList)%2 != 0:
+        replaceList.append(replaceList[-1]) 
+        
     for i in range(0, len(replaceList), 2):
         text = text.replace(replaceList[i],replaceList[i +1])
     return {
